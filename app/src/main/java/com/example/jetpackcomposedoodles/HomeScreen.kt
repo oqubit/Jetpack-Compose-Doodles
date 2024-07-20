724691b177ae7f0be19618d67dbab4d8f20886b6
o_Qubit <oqubit@users.noreply.github.com>
2024-07-20 14:33:03 +0300

    Prepare navigation

---
+++ app/src/main/java/com/example/jetpackcomposedoodles/HomeScreen.kt
@@ -0,0 +1,52 @@
+package com.example.jetpackcomposedoodles
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+
+@Composable
+fun HomeScreen(navController: NavHostController) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.6f)
+            ) {
+                Text(
+                    text = "Home Screen",
+                    style = TextStyle(
+                        brush = Brush.linearGradient(listOf(Color(0xFF0F6FFF), Color(0xFF77FFA9))),
+                        fontWeight = FontWeight.ExtraBold,
+                        fontSize = 50.sp
+                    ),
+                    modifier = Modifier.align(alignment = Alignment.Center)
+                )
+            }
+            Button(onClick = { navController.navigate(Destination.HomeScreen.route) }) {
+                Text(text = "Screen 1")
+            }
+            Button(onClick = { navController.navigate(Destination.HomeScreen.route) }) {
+                Text(text = "Screen 2")
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/jetpackcomposedoodles/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposedoodles/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.jetpackcomposedoodles.ui.theme.JetpackComposeDoodlesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +17,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpackComposeDoodlesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                NavigationAppHost(navController = navController)
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+sealed class Destination(val route: String) {
+    data object HomeScreen : Destination("home_screen")
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    JetpackComposeDoodlesTheme {
-        Greeting("Android")
-    }
+fun NavigationAppHost(navController: NavHostController) {
+   NavHost(navController= navController, startDestination = "home_screen") {
+       composable(Destination.HomeScreen.route) { HomeScreen(navController) }
+   }
 }
